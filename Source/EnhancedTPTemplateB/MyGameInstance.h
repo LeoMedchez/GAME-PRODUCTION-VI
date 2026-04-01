468dcb9d9edd93bf047e50ee8f6c40a1a49c37c8
LeoMedchez <151944849+LeoMedchez@users.noreply.github.com>
2026-04-01 12:02:53 -0600

    Added Character Capture To Show In Inventory Panel
    ~Initial Loading Screen Added To Project

---
+++ Source/EnhancedTPTemplateB/MyGameInstance.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+UCLASS()
+class ENHANCEDTPTEMPLATEB_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Init() override;
+	virtual void Shutdown() override;
+
+	UFUNCTION()
+	virtual void BeginLoadingScreen(const FString& MapName);
+
+	UFUNCTION()
+	virtual void EndLoadingScreen(UWorld* InLoadWorld);
+};
